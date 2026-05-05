b8f046b8b0db7c6383b94c54ee02bad3448c83e7
Kiicki <kulasic.igor@gmail.com>
2026-05-05 21:11:12 +0200

    asdfsadfg

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string when shipping changes that should force-refresh the cache.
-const CACHE = 'pulse-v3';
+const CACHE = 'pulse-v4';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-maskable.svg'];
 
 self.addEventListener('install', (event) => {
